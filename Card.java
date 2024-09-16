7fa29a6713d760ad21aef6e7b162de2079ce96e0
Sara-Rosee <amazongirl333@gmail.com>
2024-09-16 16:09:10 -0700

    Updated Code

---
+++ Card.java
@@ -0,0 +1,53 @@
+
+
+// Card Class notes: The Card class represents a playing card in a deck. 
+// The Card class holds all the information about a single playing card and 
+// provides a way to access and display that information.
+
+public class Card {
+    private int value;
+    private String suit;
+    private String rank;
+  
+    // Constructors
+    public Card(int value, String suit, String rank) {
+      this.value = value;
+      this.suit = suit;
+      this.rank = rank;
+    }
+  
+  // Accessor Getter Methods
+    public int getValue() {
+      return value;
+    }
+  
+    public String getSuit() {
+      return suit;
+    }
+  
+    public String getRank() {
+      return rank;
+    }
+  // allows you to change the card's value
+    public void setValue(int value) {
+      this.value = value;
+    }
+  
+    // Mutator allows you to change the card's suit
+    public void setSuit(String suit) {
+      this.suit = suit;
+    }
+  
+    // Mutator allows you to change the card's rank
+    public void setRank(String rank) {
+      this.rank = rank;
+    }
+  
+  
+    // toString method to return a string representation of the card
+    @Override
+    public String toString() {
+      return rank + " of " + suit;
+    }
+  }
+  
